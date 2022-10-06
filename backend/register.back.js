45de7456c098ff50b102ade5c50649413d18f85f
RedMedina <69237359+RedMedina@users.noreply.github.com>
2022-10-05 19:56:41 -0500

    validacion de contraseña

--- backend/register.back.js
+++ backend/register.back.js
@@ -1,7 +1,23 @@
 $(document).ready
 (
-	function($){$("#registrar").click(function(){ upUser(); })}
+	function($){$("#registrar").click(function(){ Validate(); })}
 );
+
+function Validate()
+{
+    var pass = $("#pass").val();
+    var Cpass = $("#Cpass").val();
+
+    let re = /^(?=.*\d)(?=.*[!@#$%^&.*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if(re.test(pass) && pass == Cpass)
+    {
+        upUser();
+    }
+    else
+    {
+        alert("La contraseña debe contener mínimo 8 caracteres, tener mayusculas y minisculas y un caracter especial o no concuerdan las contraseñas");
+    }
+}
 
 function upUser()
 {
